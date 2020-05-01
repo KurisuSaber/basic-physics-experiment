2aa0dcb415044cf77b81ddd04969a55b0b1b4182
ChetTian <724133160@qq.com>
2020-05-01 17:50:37 +0800

    build demo

---
+++ build/precache-manifest.6a6f6f24d71efa0de70e64d9d6ff0b3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d542402929110e3c63762f6654db654",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fb69f8f7dffb718ec0d",
+    "url": "/static/css/2.4e450864.chunk.css"
+  },
+  {
+    "revision": "73257eb3f2d01ad152e9",
+    "url": "/static/css/main.213783e3.chunk.css"
+  },
+  {
+    "revision": "3fb69f8f7dffb718ec0d",
+    "url": "/static/js/2.6eebe49b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6eebe49b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73257eb3f2d01ad152e9",
+    "url": "/static/js/main.340c6cbf.chunk.js"
+  },
+  {
+    "revision": "97423281272bfc5e2809",
+    "url": "/static/js/runtime-main.f3f79167.js"
+  }
+]);
